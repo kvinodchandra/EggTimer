1731f5ae364112d27b3a3c0f2892ec1455772299
Vinod Kumar <51116615+kvinodchandra@users.noreply.github.com>
2020-06-30 12:24:10 +0530

    Add files via upload

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by Rishikesh Agrawani on 30/06/20.
+//  Copyright © 2020 Vinod Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
